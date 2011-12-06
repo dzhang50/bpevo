8dd159e2d68f4d506c607e9829ebba66212fef7f
dzhang50 <dzhang50@a021d629-b64a-4073-a98e-9363c68f2d46>
2011-12-06 04:26:28 +0000

    Added command line parameter to run.py for number of threads
    git-svn-id: https://subversion.assembla.com/svn/bp-evo@57 a021d629-b64a-4073-a98e-9363c68f2d46

--- parser/runs/run.py
+++ parser/runs/run.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 
 import subprocess
-import os
+import os, sys
 import glob
 import time
 
 files = glob.glob('../bin/*')
 command = "./run.sh"
 processes = set()
-max_processes = 12
+max_processes = int(sys.argv[1])
 
 for name in files:
     processes.add(subprocess.Popen([command, name]))
