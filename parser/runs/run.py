ac1621de0e3133bf5811eae634bc6c80d2e8f2de
dzhang50 <dzhang50@a021d629-b64a-4073-a98e-9363c68f2d46>
2011-12-07 01:58:29 +0000

    Fixed iterator bug in run.py
    git-svn-id: https://subversion.assembla.com/svn/bp-evo@76 a021d629-b64a-4073-a98e-9363c68f2d46

--- parser/runs/run.py
+++ parser/runs/run.py
@@ -7,16 +7,18 @@ import time
 
 files = glob.glob('../bin/*')
 command = "./run.sh"
-processes = set()
+processes = list()
 max_processes = int(sys.argv[1])
 
 for name in files:
     #print name
-    processes.add(subprocess.Popen([command, os.path.basename(name)]))
+    processes.append(subprocess.Popen([command, os.path.basename(name)]))
     if len(processes) >= max_processes:
         os.wait()
-        processes.difference_update(
-            p for p in processes if p.poll() is not None)
+        for p in processes:
+            if(p.poll() is not None):
+                processes.remove(p);
+                break;
 
 #print len(processes)
 
